7b03796e9d5508c9bebf06a5c83163addfba7d57
pinalto <pinoaltotm@gmail.com>
2019-04-05 13:17:21 +0900

    feat(footer): Footerコンポーネント作成 #31

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <nuxt-link to="/privacy">
+      プライバシーポリシー
+    </nuxt-link>
+    <nuxt-link to="/developer">
+      メンバーページ
+    </nuxt-link>
+  </footer>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Footer extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
