d6d90996ea00f33e9505ebc40d44dbd407cf033f
yuFujii <32005523+yuFujii@users.noreply.github.com>
2019-04-14 08:08:36 +0900

    fix(dialog): styleのclassをまとめた #29

--- components/Dialog.vue
+++ components/Dialog.vue
@@ -1,7 +1,7 @@
 <template>
   <transition name="modal">
-    <div class="modal-mask">
-      <div class="modal-outside" @click="$emit('close')" />
+    <div>
+      <div class="modal-mask" @click="$emit('close')" />
       <div class="modal-wrapper">
         <div class="modal">
           <div class="modal__content">
@@ -23,24 +23,17 @@
 <style lang="scss" scoped>
 .modal-mask {
   position: fixed;
-  z-index: 9998;
+  z-index: 9997;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, .5);
-  transition: opacity .3s ease;
-}
-
-.modal-outside{
-  position: fixed;
-  height: 100%;
-  width: 100%;
-  z-index: -1;
 }
 
 .modal-wrapper {
-  position: absolute;
+  position: fixed;
+  z-index: 9998;
   top: 50%;
   left: 50%;
   transform: translateY(-50%) translateX(-50%);
@@ -51,13 +44,14 @@
   background-color: #fff;
   border-radius: 2px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-  transition: all .3s ease;
 }
 
 .modal-leave-active {
+  transition: .3s ease;
   opacity: 0;
   .modal {
-  transform: scale(1.1);
+    transition: .3s ease;
+    transform: scale(1.1);
   }
 }
 </style>
